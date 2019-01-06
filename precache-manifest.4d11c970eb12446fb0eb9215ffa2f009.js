1549ac129d9ed924b94c96631813bc47c7fd2697
ri7nz <ri7nz.labs@gmail.com>
2019-01-06 17:00:54 +0700

    Updates

---
+++ precache-manifest.4d11c970eb12446fb0eb9215ffa2f009.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd17c57f05481f3d3107",
+    "url": "/static/js/4.bd17c57f.chunk.js"
+  },
+  {
+    "revision": "e599935d2b16b025f471",
+    "url": "/static/js/0.e599935d.chunk.js"
+  },
+  {
+    "revision": "c73ed5c378f08aac4e17",
+    "url": "/static/js/main.c73ed5c3.chunk.js"
+  },
+  {
+    "revision": "6063455c873c10a9f97c",
+    "url": "/static/js/2.6063455c.chunk.js"
+  },
+  {
+    "revision": "0841b44b4813069055ad",
+    "url": "/static/js/3.0841b44b.chunk.js"
+  },
+  {
+    "revision": "53a775c8f76be7e30563",
+    "url": "/static/js/runtime~main.53a775c8.js"
+  },
+  {
+    "revision": "b8bb117cea1ab06dbf30",
+    "url": "/static/js/5.b8bb117c.chunk.js"
+  },
+  {
+    "revision": "c73ed5c378f08aac4e17",
+    "url": "/static/css/main.fdfeceea.chunk.css"
+  },
+  {
+    "revision": "b8bb117cea1ab06dbf30",
+    "url": "/static/css/5.0b27e275.chunk.css"
+  },
+  {
+    "revision": "4b0fbe14e0f7aedc9b72b1224de5e772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c513888c0485415eb30c4f4a91c3cd9a",
+    "url": "/1b0574649365c23f4489.worker.js"
+  }
+];
