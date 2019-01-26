8e598db213c58a93166a9d15f2366d13177e4bd9
Travis CI User <travis@example.org>
2019-01-26 06:52:08 +0000

    Updates

---
+++ precache-manifest.0560671da4d90ecef65757a61db4f1e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d3095f5cbd0011cb44",
+    "url": "/static/js/4.d9d3095f.chunk.js"
+  },
+  {
+    "revision": "fa4839eef06404819c8f",
+    "url": "/static/js/0.fa4839ee.chunk.js"
+  },
+  {
+    "revision": "b15db034d24b94e27c3e",
+    "url": "/static/js/main.b15db034.chunk.js"
+  },
+  {
+    "revision": "bae7b5cf7b3dcf1f707d",
+    "url": "/static/js/2.bae7b5cf.chunk.js"
+  },
+  {
+    "revision": "b164a4de6672738103f0",
+    "url": "/static/js/3.b164a4de.chunk.js"
+  },
+  {
+    "revision": "dac5b117c5212fd86da8",
+    "url": "/static/js/runtime~main.dac5b117.js"
+  },
+  {
+    "revision": "c862ba7a0d7a818d38b7",
+    "url": "/static/js/5.c862ba7a.chunk.js"
+  },
+  {
+    "revision": "b15db034d24b94e27c3e",
+    "url": "/static/css/main.2cd932fc.chunk.css"
+  },
+  {
+    "revision": "c862ba7a0d7a818d38b7",
+    "url": "/static/css/5.8916ddb2.chunk.css"
+  },
+  {
+    "revision": "0f42411f134bff944e2986238eb05eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "606fdfd36ea776b855781c98d37f2f95",
+    "url": "/bc219952e3fcfcfa0be9.worker.js"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88644d45e912812112b733ae7bfd10de.js"
+  "/precache-manifest.0560671da4d90ecef65757a61db4f1e9.js"
 );
 
 workbox.clientsClaim();
