defa49e796ddcb512ac2692fe444e91abb63e5c4
Rin <radysurya.agus@gmail.com>
2019-01-26 17:58:37 +0700

    [Added] builderlogo.js for generate logo (#8)
    * [Added] buildlogo.js for generate logo
    
    * [Changed] Only master & not a pull request for deploy in gh-pages and Change Title

---
+++ buildlogo.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const sharp = require('sharp')
+const listSize = [500, 192, 150, 64]
+const original = sharp('./public/logo.png')
+
+listSize.forEach(imgSize => {
+  original
+    .resize(imgSize, imgSize)
+    .toFile(`./build/logo${imgSize}.png`)
+    .then(res => {
+      console.log(`Generated Logo with Size: ${imgSize} is OK!`)
+    })
+})
