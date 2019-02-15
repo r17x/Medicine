ddf0eef8b44c5ca0feabb8e0b9a5f7041c5c85b2
ri7nz <ri7nz.labs@gmail.com>
2019-02-15 20:36:55 +0700

    added madge for generate architecture

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '**/*.+(js|json|less|css|ts|tsx|md)': [
       'prettier --write',
       'yarn test --coverage --findRelatedTests',
+      'madge src --image docs/arch.png --noDependencyColor "#ef6437" --backgroundColor "#fafafa" --nodeColor "#4d4afc"',
       'git add',
     ],
   },
